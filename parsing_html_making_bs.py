6e55ce8726faa2ff14608a196aeea5456eae39d1
bilard <bilardosinulu@gmail.com>
2023-02-02 16:38:02 -0700

    intro to bs4

--- parsing_html_making_bs.py
+++ parsing_html_making_bs.py
@@ -4,4 +4,18 @@ with open('website.html') as file:
     content = file.read()
 
 soup = BeautifulSoup(content, 'html.parser')
-print(soup.title.string)
+# print(soup.title.string)
+
+"""find_all & find are used to get a list or get a particular element based on the html tag name and other attrs of that element"""
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    # print(tag.getText(), tag.get("href"))
+    pass
+
+h3_with_heading_class = soup.find(name="h3", class_="heading")
+print(h3_with_heading_class.getText())
+
+"""select and select_one help select a list of element or a particular element based on a selector which is literally just how we target html elements in css"""
+company_url = soup.select_one(selector="p a")
+print(company_url.getText(), company_url.get("href"))
